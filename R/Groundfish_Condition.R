aafa9a45e7f62b51b5146372a0acac2a993cf8aa
Chris Rooper <chris.rooper@dfo-mpo.gc.ca>
2018-11-27 09:58:58 -0800

    Added outlier detection and removal

--- R/Groundfish_Condition.R
+++ R/Groundfish_Condition.R
@@ -9,9 +9,28 @@
 #' @examples
 #' lw.resids()
 
-lw.resids<-function(length,weight){
+lw.resids<-function(length,weight,outlier.rm=FALSE){
+ # length<-tempdata$LENGTH
+#  weight<-tempdata$WEIGHT
+ # outlier.rm<-TRUE
+  
+  require(car)
   loglength<-log(length)
   logwt<-log(weight)
-  lw.res<-lm(logwt~loglength)
-  lw.res<-lw.res$residuals
+    lw.res<-lm(logwt~loglength)
+  # Assessing Outliers using Bonferoni Outlier Test
+  #Identify if there are any outliers in your data that exceed cutoff = 0.05 (default)
+  if(outlier.rm==TRUE){
+  outlierTest(lw.res,n.max=Inf)
+    #QQ residual plot with SD's 
+  qqPlot(lw.res, main="QQ Plot") #qq plot for studentized resid
+  #Produce a bonferoni value for each point in your data
+  test1<-outlierTest(lw.res,n.max=Inf,cutoff=Inf,order=FALSE)$bonf.p 
+  remove<-which(test1<.7)
+  logwt[remove]<-NA
+  lw.res<-lm(logwt~loglength,na.action=na.exclude)
+  lw.res<-residuals(lw.res) 
+  }
+    
+ if(outlier.rm==FALSE){ lw.res<-residuals(lw.res)}
   return(lw.res)}
