6aa8bb2f059fc33ca69228dc3566681c94654cf4
Chris Rooper <chris.rooper@dfo-mpo.gc.ca>
2018-11-27 18:08:52 -0800

    Implemented weighting

--- R/Groundfish_Condition.R
+++ R/Groundfish_Condition.R
@@ -11,7 +11,7 @@
 #' @examples
 #' lw.resids()
 
-lw.resids<-function(length,weight,catch=1, outlier.rm=FALSE){
+lw.resids<-function(length,weight,outlier.rm=FALSE){
 # For testing
 # length<-tempdata$LENGTH
 # weight<-tempdata$WEIGHT
@@ -29,12 +29,46 @@ lw.resids<-function(length,weight,catch=1, outlier.rm=FALSE){
  # qqPlot(lw.res, main="QQ Plot") #qq plot for studentized resid
   #Produce a bonferoni value for each point in your data
   test1<-outlierTest(lw.res,n.max=Inf,cutoff=Inf,order=FALSE)$bonf.p 
-  remove<-which(test1<.7)
+  remove<-which(test1<.7,)
+	print("Outlier rows removed")
+	print(unname(remove))
   logwt[remove]<-NA
   lw.res<-lm(logwt~loglength,na.action=na.exclude)
   lw.res<-residuals(lw.res) 
   }
     
  if(outlier.rm==FALSE){ lw.res<-residuals(lw.res)}
-
   return(lw.res)}
+
+#' A function to weight length-weight residuals by catch
+#'
+#' This function weights length-weight residuals by a catch column. This
+#' catch can be CPUE from the tow where the fish was caught (most common) or
+#' stratum CPUE or biomass. 
+#' @param length Set of individual fish lengths
+#' @param weight Corresponding set of individual fish weights
+#' @param catch Catch for weighting condition calculation
+#' @param outlier.rm Should outliers be removed using Bonferoni test (cutoff = 0.7)
+#' @keywords length, weight, groundfish condition
+#' @export
+#' @examples
+#' weighted_resids()
+
+weighted_resids<-function(year, residuals, catch=1){
+wtlw.res<-residuals
+if(length(catch)==1){catch<-rep(1,length(residuals))}
+years1<-unique(year)
+for(i in 1:length(years1)){
+d0<-which(year==years1[i])
+d1<-residuals[d0]
+d2<-catch[d0]
+ var1<-d1*d2
+ var2<-sum(d2)
+ var3<-var1/var2*length(d2)
+wtlw.res[d0]<-var3}
+return(wtlw.res)}
+
+
+
+
+
